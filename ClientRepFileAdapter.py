f4ec037cdc67d518dea0d260b9c8cd259052d6bc
albinasivtsova15 <112868540+albinasivtsova15@users.noreply.github.com>
2025-02-07 19:37:30 +0300

    Lab. 2.6 Creating class Adapter
    Создания класса адаптера для того, чтобы интерфейс был одинаковый при работе с файлами и работе с БД

---
+++ ClientRepFileAdapter.py
@@ -0,0 +1,28 @@
+import ClientRepFile
+
+class ClientRepFileAdapter:
+    
+    def __init__(self, client_rep_file: ClientRepFile):
+        client_rep_file.read_data_from_file()
+        self._client_rep_file = client_rep_file
+    
+    def get_k_n_short_list(self, k, n):
+        return self._client_rep_file.get_k_n_short_list(k, n)
+    
+    def get_by_id(self, client_id):
+        return self._client_rep_file.get_by_id(client_id)
+    
+    def delete_by_id(self, client_id):
+        self._client_rep_file.delete_by_id(client_id)
+        self._client_rep_file.write_data_to_file()
+    
+    def replace_by_id(self, client_id, company_name, contact_person, email, phone, passport):
+        self._client_rep_file.replace_by_id(client_id, company_name, contact_person, email, phone, passport)
+        self._client_rep_file.write_data_to_file()
+    
+    def add_entity(self, company_name, contact_person, email, phone, passport):
+        self._client_rep_file.add_entity(company_name, contact_person, email, phone, passport)
+        self._client_rep_file.write_data_to_file()
+    
+    def get_count(self):
+        return self._client_rep_file.get_count()
